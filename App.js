dc71484a200ea7437d50d3592c06f1b7d49d5c61
Lillian Dixon <dixon.lillian92@gmail.com>
2020-10-29 10:29:07 -0600

    initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from "./src/screens/HomeScreen";
+
+import ComponentsScreen from "./src/screens/componentsScreen";
+import ListScreen from "./src/screens/listScreens";
+import ImageScreens from "./src/screens/ImageScreens"
+import CounterScreen from './src/screens/CounterScreen';
+import ColorScreen from './src/screens/ColorScreen';
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Components: ComponentsScreen,
+    List: ListScreen,
+    Image: ImageScreens,
+    Counter: CounterScreen,
+    Color: ColorScreen
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "App"
+    }
+  }
+);
+
+export default createAppContainer(navigator);
